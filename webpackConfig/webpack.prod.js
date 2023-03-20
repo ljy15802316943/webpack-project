55d85e0eaffa6a4ad5b7b66b04a1d57b6cd78364
黎江杨 <jiangyang.li@macrowing.com>
2023-03-20 16:35:20 +0800

    添加打包配置压缩代码，删除多余的代码

--- webpackConfig/webpack.prod.js
+++ webpackConfig/webpack.prod.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const webpackConfig = require('./webpack.config');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(webpackConfig, {
   mode: 'production',
+  optimization: {
+    //压缩代码，删除多余的代码
+    minimizer: [new TerserPlugin({ /* additional options here */ })],
+  },
 });
