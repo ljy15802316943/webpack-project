7ad3ee108d4798630ea6f15d584539ab9a0f17bb
黎江杨 <jiangyang.li@macrowing.com>
2023-03-24 16:15:17 +0800

    添加代理

--- server/app.js
+++ server/app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const CONST = require('./const');
 
 const app = express();
+
+let inwiseUrl = 'http://inwise.macrowing.com:8089';
+app.use(
+  createProxyMiddleware('/api', {
+    target: inwiseUrl, 
+    changeOrigin: true,
+    onProxyRes(proxyRes) {
+      proxyRes.headers['x-real-url'] = inwiseUrl;
+    },
+    pathRewrite: {
+      '^/api': '', // rewrite path
+    },
+  }),
+);
 
 //获取node-webpack配置
 const compiler = webpack(require('./server.webpack.config'));
